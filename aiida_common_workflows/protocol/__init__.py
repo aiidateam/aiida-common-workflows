d44e3d06c3d2455e8024d05012a99b76f6c09f6b
Sebastiaan Huber <mail@sphuber.net>
2020-05-06 12:29:12 +0200

    Add the `ProtocolRegistry` utility class
    This class implements the basic interface to define a set of protocols
    for a given workflow. It exposes some methods to request the available
    protocols and the default. This class can be used for composition when
    defining an `InputGenerator`, that will define specific protocols for a
    particular common workflow.

---
+++ aiida_common_workflows/protocol/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Module with utilities to define and implement workchain protocols."""
+from .registry import *
+
+__all__ = (registry.__all__,)
---
+++ tests/protocol/test_registry.py
@@ -0,0 +1,82 @@
+import pytest
+
+from aiida_common_workflows.protocol import ProtocolRegistry
+
+
+@pytest.fixture
+def protocol_registry():
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = {'efficiency': {'description': 'description'}, 'precision': {'description': 'description'}}
+        _default_protocol = 'efficiency'
+
+    return SubProtocolRegistry()
+
+
+def test_validation():
+    """Test the validation of subclasses of `ProtocolRegistry`."""
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = None
+        _default_protocol = None
+
+    with pytest.raises(RuntimeError):
+        SubProtocolRegistry()
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = {'efficiency': {}, 'precision': 'wrong_type'}
+        _default_protocol = None
+
+    with pytest.raises(RuntimeError):
+        SubProtocolRegistry()
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = {'efficiency': {'description': 'description'}, 'precision': {}}
+        _default_protocol = None
+
+    with pytest.raises(RuntimeError):
+        SubProtocolRegistry()
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = {'efficiency': {'description': 'description'}, 'precision': {'description': 'description'}}
+        _default_protocol = None
+
+    with pytest.raises(RuntimeError):
+        SubProtocolRegistry()
+
+    class SubProtocolRegistry(ProtocolRegistry):
+
+        _protocols = {'efficiency': {'description': 'description'}, 'precision': {'description': 'description'}}
+        _default_protocol = 'non-existant'
+
+    with pytest.raises(RuntimeError):
+        SubProtocolRegistry()
+
+
+def test_is_valid_protocol(protocol_registry):
+    """Test `ProtocolRegistry.is_valid_protocol`."""
+    assert protocol_registry.is_valid_protocol('efficiency')
+    assert not protocol_registry.is_valid_protocol('non-existant')
+
+
+def test_get_protocol_names(protocol_registry):
+    """Test `ProtocolRegistry.get_protocol_names`."""
+    assert protocol_registry.get_protocol_names() == ['efficiency', 'precision']
+
+
+def test_get_default_protocol_name(protocol_registry):
+    """Test `ProtocolRegistry.get_default_protocol_name`."""
+    assert protocol_registry.get_default_protocol_name() == 'efficiency'
+
+
+def test_get_protocol(protocol_registry):
+    """Test `ProtocolRegistry.get_protocol`."""
+    assert protocol_registry.get_protocol('efficiency') == {'description': 'description'}
+
+    with pytest.raises(ValueError):
+        protocol_registry.get_protocol('non-existant')
