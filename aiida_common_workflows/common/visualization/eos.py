e0fcd8fb5d2e3ba8443dcebfd38f69df65ccae89
Sebastiaan Huber <mail@sphuber.net>
2020-10-13 09:15:04 +0200

    Implement the common Equation of State workflow (#35)
    This `WorkChain` employs the available implemenations of the
    `CommonRelaxWorkChain` to compute the equation of state of a given
    crystal structure. The relax workflow that is to be used is communicated
    through the `sub_process_class` input, which expects its entry point name.
    
    The `EquationOfStateWorkChain` will launch the sub process for the input
    structure that is first scaled at a list of scaling factors. These can
    either be specified explicitly as `scaling_factors`, or by default they
    will be generated by the work chain itself. The number of scaling
    factors and the relative increment can be controlled with the inputs
    `scale_count` and `scale_increment`, respectively. The EOS workflow will
    simply return the relaxed structures and total energies computed by the
    common relax workflows.
    
    The work chain spec does not actually expose the inputs of the common
    relax workflow that will be used as the executor, because the specific
    plugin implementation that is to be used will only become known when the
    work chain is launched, i.e., when an instance is created. This prevents
    the classmethod define to expose the inputs. On top of that, the
    workchain itself will have to call the inputs generator of the plugin
    implementation that is to be used in order to generate the inputs. One
    would think this could be done once before calling the EOS workchain and
    but that is not possible. Inside the workchain, the inner workchain has
    to be called multiple times with a different structure, but the outer
    workchain cannot now where in the input hierarchy the input structure
    should be passed. Not all inner implementations necessarily expose the
    structure port at the top level. Therefore the outer workchain has to
    call the generator.
    
    Another reason is that the input generator is potentially dependent on
    a previously run workchain, through the `previous_workchain` keyword.
    This is because for an EOS to make sense, certain implementations will
    need to keep certain variables constant across the various calculations.
    This again means the workchain will need to call the generator inside
    the workchain and we cannot simply generate the inputs outside the
    workchain and pass this is.
    
    This all means that the inputs that are supposed to be passed to the
    generator, also need to be passed to the workchain itself. For now we
    add a port `generator_inputs` that takes a dictionary of the values that
    are to be passed to the input generators. Maybe we want to make this a
    `non_db` portnamespace in the future, since it doesn't really need to be
    part of the provenance, just as normally calling the input generator is
    not part of the provenance, which is fine.
    
    The workchain still defines a dynamic input namespace `sub_process`.
    This is completely optional but any input passed here will override the
    inputs created by the generator. This allows a caller to override
    certain inputs should they want to. It is not exactly clear yet how this
    will work and if it will be practical.

---
+++ aiida_common_workflows/common/visualization/eos.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Utilities to fit and visualize a Equation of States based on set of volumes and energies."""
+import typing
+
+import numpy
+
+
+def birch_murnaghan(V, E0, V0, B0, B01):
+    """Compute energy by Birch Murnaghan formula."""
+    # pylint: disable=invalid-name
+    r = (V0 / V)**(2. / 3.)
+    return E0 + 9. / 16. * B0 * V0 * (r - 1.)**2 * (2. + (B01 - 4.) * (r - 1.))
+
+
+def fit_birch_murnaghan_params(volumes, energies):
+    """Fit Birch Murnaghan parameters."""
+    # pylint: disable=invalid-name
+    from scipy.optimize import curve_fit
+
+    params, covariance = curve_fit(  # pylint: disable=unbalanced-tuple-unpacking
+        birch_murnaghan,
+        xdata=volumes,
+        ydata=energies,
+        p0=(
+            energies.min(),  # E0
+            volumes.mean(),  # V0
+            0.1,  # B0
+            3.,  # B01
+        ),
+        sigma=None
+    )
+    return params, covariance
+
+
+def plot_eos(
+    volumes: typing.List[float], energies: typing.List[float], unit_volume: str = 'Å^3', unit_energy: str = 'eV'
+):
+    """Plot the Equation of State for a given set of volumes and energies
+
+    :param volumes: list of cell volumes.
+    :param energies: list of energies.
+    :param unit_volume: unit of volume, default is [Å^3].
+    :param unit_energy: unit of energy, default is [eV].
+    """
+    import pylab as plt
+
+    params, _ = fit_birch_murnaghan_params(numpy.array(volumes), numpy.array(energies))
+
+    volume_min = min(volumes)
+    volume_max = max(volumes)
+    volume_range = numpy.linspace(volume_min, volume_max, 300)
+
+    plt.plot(volumes, energies, 'o')
+    plt.plot(volume_range, birch_murnaghan(volume_range, *params))
+
+    plt.xlabel(f'Volume [{unit_volume}]')
+    plt.ylabel(f'Energy [{unit_energy}]')
+    plt.show()
--- tests/cli/test_launch.py
+++ tests/cli/test_launch.py
@@ -3,7 +3,7 @@
 import click
 import pytest
 
-from aiida_common_workflows.cli.launch import cmd_relax
+from aiida_common_workflows.cli.launch import cmd_relax, cmd_eos
 
 
 @pytest.mark.usefixtures('aiida_profile')
@@ -28,5 +28,31 @@ def test_relax_number_machines(run_cli_command, generate_structure, generate_cod
     # Passing two values for `-m` should raise as only one value is required
     options = ['-S', str(structure.pk), '-m', '100', '100', '--', 'quantum_espresso']
     result = run_cli_command(cmd_relax, options, raises=click.BadParameter)
+    assert 'Error: Invalid value for --number-machines: QuantumEspressoRelaxWorkChain has 1 engine steps, so ' \
+           'requires 1 values' in result.output_lines
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_eos_wallclock_seconds(run_cli_command, generate_structure, generate_code):
+    """Test the `--wallclock-seconds` option."""
+    structure = generate_structure().store()
+    generate_code('quantumespresso.pw').store()
+
+    # Passing two values for `-w` should raise as only one value is required
+    options = ['-S', str(structure.pk), '-w', '100', '100', '--', 'quantum_espresso']
+    result = run_cli_command(cmd_eos, options, raises=click.BadParameter)
+    assert 'Error: Invalid value for --wallclock-seconds: QuantumEspressoRelaxWorkChain has 1 engine steps, so ' \
+           'requires 1 values' in result.output_lines
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_eos_number_machines(run_cli_command, generate_structure, generate_code):
+    """Test the `--number-machines` option."""
+    structure = generate_structure().store()
+    generate_code('quantumespresso.pw').store()
+
+    # Passing two values for `-m` should raise as only one value is required
+    options = ['-S', str(structure.pk), '-m', '100', '100', '--', 'quantum_espresso']
+    result = run_cli_command(cmd_eos, options, raises=click.BadParameter)
     assert 'Error: Invalid value for --number-machines: QuantumEspressoRelaxWorkChain has 1 engine steps, so ' \
            'requires 1 values' in result.output_lines
