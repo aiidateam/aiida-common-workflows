d663cd64dfe995fd6b92afff60026c584978496e
Samuel Poncé <samuel.pon@gmail.com>
2020-11-05 15:33:56 +0100

    Abinit Common Workflows (#58)
    Abinit implementation of common workflows
    
    Co-authored-by: Austin Zadoks <zooks97@gmail.com>
    Co-authored-by: Guido Petretto <g.petretto@gmail.com>
    Co-authored-by: Austin Zadoks <austin.zadoks@protonmail.com>

---
+++ aiida_common_workflows/workflows/relax/abinit/workchain.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Implementation of `aiida_common_workflows.common.relax.workchain.CommonRelaxWorkChain` for Abinit."""
+from aiida import orm
+from aiida.engine import calcfunction
+from aiida_abinit.workflows.base import AbinitBaseWorkChain
+
+from ..workchain import CommonRelaxWorkChain
+from .generator import AbinitRelaxInputsGenerator
+
+__all__ = ('AbinitRelaxWorkChain',)
+
+
+@calcfunction
+def get_stress_from_trajectory(trajectory):
+    """Return the stress array from the given trajectory data."""
+    stress = orm.ArrayData()
+    stress.set_array(name='stress', array=trajectory.get_array('stress')[-1])
+    return stress
+
+
+@calcfunction
+def get_forces_from_trajectory(trajectory):
+    """Return the forces array from the given trajectory data."""
+    forces = orm.ArrayData()
+    forces.set_array(name='forces', array=trajectory.get_array('forces')[-1])
+    return forces
+
+
+@calcfunction
+def get_total_energy(parameters):
+    """Return the total energy from the given parameters node."""
+    return orm.Float(parameters.get_attribute('energy'))
+
+
+class AbinitRelaxWorkChain(CommonRelaxWorkChain):
+    """Implementation of `aiida_common_workflows.common.relax.workchain.CommonRelaxWorkChain` for Abinit."""
+
+    _process_class = AbinitBaseWorkChain
+    _generator_class = AbinitRelaxInputsGenerator
+
+    def convert_outputs(self):
+        """Convert the outputs of the sub workchain to the common output specification."""
+        self.out('relaxed_structure', self.ctx.workchain.outputs.output_structure)
+        self.out('total_energy', get_total_energy(self.ctx.workchain.outputs.output_parameters))
+        self.out('forces', get_forces_from_trajectory(self.ctx.workchain.outputs.output_trajectory))
+        self.out('stress', get_stress_from_trajectory(self.ctx.workchain.outputs.output_trajectory))
