b0787fa27af64506d0bae70123a5d24e11e8023f
Jens Broeder <broeder-j@users.noreply.github.com>
2021-01-14 12:53:37 +0100

    Abort `EosWorkChain` and `DCWorkChain` if init run fails (#121)
    There is no need to run all other steps in these workchains if the first
    initialization workchain fails, since all have to finish successful for the
    final result. The workchain is aborted in this case to avoid unnecessary
    resources being wasted.

--- aiida_common_workflows/workflows/eos.py
+++ aiida_common_workflows/workflows/eos.py
@@ -98,6 +98,7 @@ class EquationOfStateWorkChain(WorkChain):
         spec.inputs.validator = validate_inputs
         spec.outline(
             cls.run_init,
+            cls.inspect_init,
             cls.run_eos,
             cls.inspect_eos,
         )
@@ -141,6 +142,12 @@ class EquationOfStateWorkChain(WorkChain):
         self.ctx.previous_workchain = self.submit(builder)
         self.ctx.structures = [structure]
         self.to_context(children=append_(self.ctx.previous_workchain))
+
+    def inspect_init(self):
+        """Check that the first workchain finished successfully or abort the workchain."""
+        if not self.ctx.children[0].is_finished_ok:
+            self.report('Initial sub process did not finish successful so aborting the workchain.')
+            return self.exit_codes.ERROR_SUB_PROCESS_FAILED.format(cls=self.inputs.sub_process_class)  # pylint: disable=no-member
 
     def run_eos(self):
         """Run the sub process at each scale factor to compute the structure volume and total energy."""
