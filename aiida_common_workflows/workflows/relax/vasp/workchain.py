5c3763cb43d66eea8acbd1042ad7f17de8357c10
Espen <espen.flage-larsen@sintef.no>
2021-02-19 18:10:35 +0100

    WIP: Updated VASP interface (#119)
    * Updated VASP interface
    
    In order to comply with recent upgrade of specifications, we here add the required translation to the VASP workchain stack for performing relaxations, simple equation of state calculations and a dissociation example. Since VASP requires a dedicated version for non-collinear calculations we do not include this or spin-orbit as an option at this point. We also do not make use of our convergence workchain in order to keep it as close to code as possible.
    
    * Added ElectronicType definitions to pass, but not used.

--- aiida_common_workflows/workflows/relax/vasp/workchain.py
+++ aiida_common_workflows/workflows/relax/vasp/workchain.py
@@ -3,6 +3,7 @@
 from aiida import orm
 from aiida.engine import calcfunction
 from aiida.plugins import WorkflowFactory
+from aiida.common.exceptions import NotExistentAttributeError
 
 from ..workchain import CommonRelaxWorkChain
 from .generator import VaspRelaxInputsGenerator
@@ -15,6 +16,7 @@ def get_stress(stress):
     """Return the final stress array."""
     stress_data = orm.ArrayData()
     stress_data.set_array(name='stress', array=stress.get_array('final'))
+
     return stress_data
 
 
@@ -23,15 +25,32 @@ def get_forces(forces):
     """Return the final forces array.."""
     forces_data = orm.ArrayData()
     forces_data.set_array(name='forces', array=forces.get_array('final'))
+
     return forces_data
 
 
 @calcfunction
-def get_total_energy(misc):
-    """Return the total energy from misc."""
-    misc_dict = misc.get_dict()
-    total_energy = orm.Float(misc_dict['total_energies']['energy_no_entropy'])
+def get_total_extrapolated_energy(energies):
+    """Return the total extrapolated energy from the energies array."""
+    # We extract by default the energies of the last electronic step, unless special flags are set.
+    total_extrapolated_energy = energies.get_array('energy_extrapolated')[0]
+    total_energy = orm.Float(total_extrapolated_energy)
+
     return total_energy
+
+
+@calcfunction
+def get_total_cell_magnetic_moment(misc):
+    """Return the total cell magnetic moment."""
+    magnetization = misc.get_dict()['magnetization']
+    if not magnetization:
+        # If list is empty, we have no magnetization
+        magnetization = 0.0
+    else:
+        # Assume we do not run non-collinear
+        magnetization = magnetization[0]
+
+    return orm.Float(magnetization)
 
 
 class VaspRelaxWorkChain(CommonRelaxWorkChain):
@@ -42,7 +61,14 @@ class VaspRelaxWorkChain(CommonRelaxWorkChain):
 
     def convert_outputs(self):
         """Convert the outputs of the sub workchain to the common output specification."""
-        self.out('relaxed_structure', self.ctx.workchain.outputs.relax__structure)
-        self.out('total_energy', get_total_energy(self.ctx.workchain.outputs.misc))
+        try:
+            self.out('relaxed_structure', self.ctx.workchain.outputs.relax__structure)
+        except NotExistentAttributeError:
+            # We have no control of when we want to perform relaxations here,
+            # this is up to the calling workchains, so do not set the relaxed structure if a
+            # relaxation was not requested.
+            pass
+        self.out('total_magnetization', get_total_cell_magnetic_moment(self.ctx.workchain.outputs.misc))
+        self.out('total_energy', get_total_extrapolated_energy(self.ctx.workchain.outputs.energies))
         self.out('forces', get_forces(self.ctx.workchain.outputs.forces))
         self.out('stress', get_stress(self.ctx.workchain.outputs.stress))
