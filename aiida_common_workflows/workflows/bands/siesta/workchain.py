69468a77d30af4a0d7f03bfbe27e5ea4a605dbca
Emanuele Bosoni <38431855+bosonie@users.noreply.github.com>
2021-12-16 19:03:39 +0100

    Add the `fermi_energy` as output of the `CommonBandsWorkChain` (#255)

--- aiida_common_workflows/workflows/bands/siesta/workchain.py
+++ aiida_common_workflows/workflows/bands/siesta/workchain.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 """Implementation of `aiida_common_workflows.common.relax.workchain.CommonRelaxWorkChain` for SIESTA."""
+from aiida.engine import calcfunction
+from aiida.orm import Float
 from aiida.plugins import WorkflowFactory
 
 from ..workchain import CommonBandsWorkChain
 from .generator import SiestaCommonBandsInputGenerator
 
 __all__ = ('SiestaCommonBandsWorkChain',)
+
+
+@calcfunction
+def get_fermi_energy(pardict):
+    """Extract the Fermi energy from the `output_parameters` dictionary"""
+    return Float(pardict['E_Fermi'])
 
 
 class SiestaCommonBandsWorkChain(CommonBandsWorkChain):
@@ -19,4 +27,7 @@ class SiestaCommonBandsWorkChain(CommonBandsWorkChain):
         if 'bands' not in self.ctx.workchain.outputs:
             self.report('SiestaBaseWorkChain concluded without returning bands!')
             return self.exit_codes.ERROR_SUB_PROCESS_FAILED
+
+        self.out('fermi_energy', get_fermi_energy(self.ctx.workchain.outputs.output_parameters))
+
         self.out('bands', self.ctx.workchain.outputs['bands'])
