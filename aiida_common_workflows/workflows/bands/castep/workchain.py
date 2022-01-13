8d01573d9ca2a85d9307a794a553c32bf7adf30d
Bonan Zhu <zhubonan@outlook.com>
2022-01-13 18:03:07 +0000

    Fix typo

--- aiida_common_workflows/workflows/bands/castep/workchain.py
+++ aiida_common_workflows/workflows/bands/castep/workchain.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Implementation of `aiida_common_workflows.common.relax.workchain.CommonRelaxWorkChain` for CASTEP."""
+from logging import getLogger
+
 from aiida.engine import calcfunction
 from aiida.orm import Float
 from aiida.plugins import WorkflowFactory
@@ -7,24 +9,22 @@ from aiida.plugins import WorkflowFactory
 from ..workchain import CommonBandsWorkChain
 from .generator import CastepCommonBandsInputGenerator
 
-from logging import getLogger
-logger = getLogger(__name__)
-
 __all__ = ('CastepCommonBandsWorkChain',)
 
 
 @calcfunction
 def get_fermi_energy(bands):
     """Extract the Fermi energy from the BandsData output"""
-    efermi = bands.get_attribute("efermi")
+    efermi = bands.get_attribute('efermi')
     if isinstance(efermi, list):
         efermi = efermi[0]
-        logger.warn("Spin polarised calculation - using the efermi energy of the first spin channel.")
+        logger = getLogger(__name__)
+        logger.warning('Spin polarised calculation - using the efermi energy of the first spin channel.')
     return Float(efermi)
 
 
 class CastepCommonBandsWorkChain(CommonBandsWorkChain):
-    """Implementation of `aiida_common_workflows.common.bands.workchain.CommonBandsWorkChain` for SIESTA."""
+    """Implementation of `aiida_common_workflows.common.bands.workchain.CommonBandsWorkChain` for CASTEP."""
 
     _process_class = WorkflowFactory('castep.bands')
     _generator_class = CastepCommonBandsInputGenerator
@@ -38,3 +38,4 @@ class CastepCommonBandsWorkChain(CommonBandsWorkChain):
         self.out('fermi_energy', get_fermi_energy(self.ctx.workchain.outputs['band_structure']))
 
         self.out('bands', self.ctx.workchain.outputs['band_structure'])
+        return None
