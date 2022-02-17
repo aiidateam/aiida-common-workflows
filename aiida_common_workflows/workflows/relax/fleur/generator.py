4021359a35a7dba827f8c94ed0c9c309df79a856
janssenhenning <henning.janssen@gmx.net>
2022-02-17 11:03:33 +0100

    Add `verification-pbe-v1` protocol

--- aiida_common_workflows/workflows/relax/fleur/generator.py
+++ aiida_common_workflows/workflows/relax/fleur/generator.py
@@ -57,7 +57,7 @@ class FleurCommonRelaxInputGenerator(CommonRelaxInputGenerator):
         spec.inputs['spin_type'].valid_type = ChoiceType((SpinType.NONE, SpinType.COLLINEAR))
         spec.inputs['relax_type'].valid_type = ChoiceType((RelaxType.NONE, RelaxType.POSITIONS))
         spec.inputs['electronic_type'].valid_type = ChoiceType((ElectronicType.METAL, ElectronicType.INSULATOR))
-        spec.inputs['protocol'].valid_type = ChoiceType(('fast', 'moderate', 'precise', 'oxides_validation'))
+        spec.inputs['protocol'].valid_type = ChoiceType(('fast', 'moderate', 'precise', 'oxides_validation', 'verification-pbe-v1'))
         spec.input('engines.inpgen.code', valid_type=orm.Code, serializer=orm.load_code)
         spec.input('engines.inpgen.options', valid_type=dict, required=False)
         spec.inputs['engines']['relax']['code'].valid_type = CodeType('fleur.fleur')
