611c2e6ce11c6e9705e3a66e4a43a9e574dc1dd1
Sebastiaan Huber <mail@sphuber.net>
2023-02-06 10:29:05 +0100

    CLI: Add entry point for CLI as `acwf` (#297)
    Currently the CLI executable is called `aiida-common-workflows` which is
    quite long. Now that we have introduced ACWF as the official acronym for
    the project, the CLI should also be invokable as `acwf`. To not break
    backwards compatibility `aiida-common-workflows` is kept along side for
    now.

--- aiida_common_workflows/cli/root.py
+++ aiida_common_workflows/cli/root.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Command line interface `aiida-common-workflows`."""
+"""Command line interface ``acwf``."""
 from aiida.cmdline.params import options, types
 import click
 
 
-@click.group('aiida-common-workflows', context_settings={'help_option_names': ['-h', '--help']})
+@click.group('acwf', context_settings={'help_option_names': ['-h', '--help']})
 @options.PROFILE(type=types.ProfileParamType(load_profile=True))
 def cmd_root(profile):  # pylint: disable=unused-argument
-    """CLI for the `aiida-common-workflows` plugin."""
+    """CLI for the ``aiida-common-workflows`` plugin."""
