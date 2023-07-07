e7d49d991791b6a5102425ca074111ab158fdfa7
Espen Flage-Larsen <espen.flage-larsen@sigma2.no>
2023-07-07 21:51:37 +0200

    Update VASP part to comply with ACWF study
    We add dedicated handler overrides and a dedicated protocol that was used for this study.
    Also, a dedicated potential mapping has been created as the VASP developers was in the
    process of updating a few potentials, in particular for the lanthanides.
    Furthermore, we disable the present default to hard stop on CNORMN warnings for this study.
    This goes in hand with the dedicated `v2.2.0` release of the AiiDA-VASP plugin, which is compatible
    with `aiida_core<2`. This commit pins the AiiDA-VASP plugin to the `v2.2.0` version.

---
+++ aiida_common_workflows/workflows/relax/vasp/extractors.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Collects some functions to postprocess a `VaspCommonRelaxWorkChain`.
+"""
+
+
+def get_ts_energy(common_relax_workchain):
+    """
+    Extract and return the energy contribution from the entropy term TS value.
+
+    The fictitious temperature is T due to the presence of a smearing and S is the entropy.
+    """
+    from aiida.common import LinkType
+    from aiida.orm import WorkChainNode
+    from aiida.plugins import WorkflowFactory
+
+    if not isinstance(common_relax_workchain, WorkChainNode):
+        return ValueError('The input is not a workchain (instance of `WorkChainNode`)')
+    if common_relax_workchain.process_class != WorkflowFactory('common_workflows.relax.vasp'):
+        return ValueError('The input workchain is not a `VaspCommonRelaxWorkChain`')
+
+    vasp_wc = common_relax_workchain.get_outgoing(link_type=LinkType.CALL_WORK).one().node
+    energies = vasp_wc.outputs.energies
+    energy_free = energies.get_array('energy_free_electronic')[0]
+    energy_no_entropy = energies.get_array('energy_no_entropy')[0]
+    energy_entropy_contrib = energy_no_entropy - energy_free
+
+    return energy_entropy_contrib
