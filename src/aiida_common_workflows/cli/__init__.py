7bf1ce57a229f58ab62247bd2f330065a310db12
Sebastiaan Huber <mail@sphuber.net>
2024-02-26 22:42:28 +0100

    Dependencies: Add explicit `click` dependency
    Removing explicit `click_completion()` which is no longer needed for
    modern versions of click.

--- src/aiida_common_workflows/cli/__init__.py
+++ src/aiida_common_workflows/cli/__init__.py
@@ -1,5 +1,1 @@
 """Module for the command line interface."""
-import click_completion
-
-# Activate the completion of parameter types provided by the click_completion package
-click_completion.init()
